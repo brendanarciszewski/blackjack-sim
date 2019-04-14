02b85aa5135d91f53cb050de7c1f61559d2ba7d3
Brendan Arciszewski <git@brendanarciszewski.ca>
2019-04-14 00:49:48 -0400

    indents in spaces

--- src/deck.rs
+++ src/deck.rs
@@ -1,96 +1,96 @@
 #[derive(Debug, PartialEq)]
 enum Suit {
-	Heart,
-	Diamond,
-	Spade,
-	Club
+    Heart,
+    Diamond,
+    Spade,
+    Club
 }
 
 #[derive(Debug, PartialEq, Clone)]
 enum Value {
-	Ace = 1,
-	Two,
-	Three,
-	Four,
-	Five,
-	Six,
-	Seven,
-	Eight,
-	Nine,
-	Ten,
-	Jack,
-	Queen,
-	King
+    Ace = 1,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King
 }
 
 impl Value {
-	fn val(&self) -> i32 {
-		match self {
-			Value::Jack | Value::Queen | Value::King => 10 as i32,
-			x => x.clone() as i32 
-		}
-	}
+    fn val(&self) -> i32 {
+        match self {
+            Value::Jack | Value::Queen | Value::King => 10 as i32,
+            x => x.clone() as i32 
+        }
+    }
 }
 
 #[derive(Debug, PartialEq)]
 pub struct Card {
-	value: Value,
-	suit: Suit
+    value: Value,
+    suit: Suit
 }
 
 impl Card {
-	pub fn val(&self) -> i32 {
-		self.value.val()
-	}
+    pub fn val(&self) -> i32 {
+        self.value.val()
+    }
 }
 
 #[derive(Debug)]
 pub struct Deck {
-	cards: Vec<Card>
+    cards: Vec<Card>
 }
 
 impl Deck {
-	pub fn new() -> Self {
-		let mut cards = vec![];
-		for num_s in 1..5 {
-			for num_v in 1..14 {
-				let value = match num_v {
-					1 => Value::Ace,
-					2 => Value::Two,
-					3 => Value::Three,
-					4 => Value::Four,
-					5 => Value::Five,
-					6 => Value::Six,
-					7 => Value::Seven,
-					8 => Value::Eight,
-					9 => Value::Nine,
-					10 => Value::Ten,
-					11 => Value::Jack,
-					12 => Value::Queen,
-					13 => Value::King,
-					x => panic!("Card {} out of range!", x)
-				};
+    pub fn new() -> Self {
+        let mut cards = vec![];
+        for num_s in 1..5 {
+            for num_v in 1..14 {
+                let value = match num_v {
+                    1 => Value::Ace,
+                    2 => Value::Two,
+                    3 => Value::Three,
+                    4 => Value::Four,
+                    5 => Value::Five,
+                    6 => Value::Six,
+                    7 => Value::Seven,
+                    8 => Value::Eight,
+                    9 => Value::Nine,
+                    10 => Value::Ten,
+                    11 => Value::Jack,
+                    12 => Value::Queen,
+                    13 => Value::King,
+                    x => panic!("Card {} out of range!", x)
+                };
 
-				let suit = match num_s {
-					1 => Suit::Heart,
-					2 => Suit::Diamond,
-					3 => Suit::Spade,
-					4 => Suit::Club,
-					x => panic!("Suit {} out of range!", x)
-				};
-				cards.push(Card {value, suit});
-			}
-		}
-		
-		use rand::prelude::*;
-		let mut rng = thread_rng();
-		cards.shuffle(&mut rng);
+                let suit = match num_s {
+                    1 => Suit::Heart,
+                    2 => Suit::Diamond,
+                    3 => Suit::Spade,
+                    4 => Suit::Club,
+                    x => panic!("Suit {} out of range!", x)
+                };
+                cards.push(Card {value, suit});
+            }
+        }
+        
+        use rand::prelude::*;
+        let mut rng = thread_rng();
+        cards.shuffle(&mut rng);
 
-		assert_eq!(cards.len(), 52);
-		Deck {cards}
-	}
+        assert_eq!(cards.len(), 52);
+        Deck {cards}
+    }
 
-	pub fn pick(&mut self) -> Option<Card> {
-		self.cards.pop()
-	}
+    pub fn pick(&mut self) -> Option<Card> {
+        self.cards.pop()
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,40 +5,40 @@ mod deck;
 use deck::Deck;
 
 fn main() -> Result<(), Box<dyn Error>> {
-	const TRIALS: i32 = 1000;
-	let mut wtr = Writer::from_path("out.csv")?;
-	wtr.write_record(&["Player's 1st Card",
-					   "Player's 1st Card Value",
-					   "Dealer's 1st Card",
-					   "Dealer's 1st Card Value",
-					   "Player's 2nd Card",
-					   "Player's 2nd Card Value",
-					   "Dealer's 2nd Card",
-					   "Dealer's 2nd Card Value",
-					   "Player's Sum",
-					   "Dealer's Sum"])?;
-	for _ in 0..TRIALS {
-		let mut deck = Deck::new();
-		let mut player = vec![];
-		let mut dealer = vec![];
-		for _ in 0..2 {
-			player.push(deck.pick().unwrap());
-			dealer.push(deck.pick().unwrap());
-		}
-		let player_sum: i32 = player.iter().map(|a| a.val()).sum();
-		let dealer_sum: i32 = dealer.iter().map(|a| a.val()).sum();
-		let rec = vec![format!("{:?}", player[0]),
-					   format!("{}", player[0].val()),
-					   format!("{:?}", dealer[0]),
-					   format!("{}", dealer[0].val()),
-					   format!("{:?}", player[1]),
-					   format!("{}", player[1].val()), 
-					   format!("{:?}", dealer[1]),
-					   format!("{}", dealer[1].val()),
-					   format!("{}", player_sum),
-					   format!("{}", dealer_sum)];
-		wtr.write_record(&rec)?;
-		//println!("{:?}\n{:?}\n", player, dealer);
-	}
-	Ok(())
+    const TRIALS: i32 = 1000;
+    let mut wtr = Writer::from_path("out.csv")?;
+    wtr.write_record(&["Player's 1st Card",
+                       "Player's 1st Card Value",
+                       "Dealer's 1st Card",
+                       "Dealer's 1st Card Value",
+                       "Player's 2nd Card",
+                       "Player's 2nd Card Value",
+                       "Dealer's 2nd Card",
+                       "Dealer's 2nd Card Value",
+                       "Player's Sum",
+                       "Dealer's Sum"])?;
+    for _ in 0..TRIALS {
+        let mut deck = Deck::new();
+        let mut player = vec![];
+        let mut dealer = vec![];
+        for _ in 0..2 {
+            player.push(deck.pick().unwrap());
+            dealer.push(deck.pick().unwrap());
+        }
+        let player_sum: i32 = player.iter().map(|a| a.val()).sum();
+        let dealer_sum: i32 = dealer.iter().map(|a| a.val()).sum();
+        let rec = vec![format!("{:?}", player[0]),
+                       format!("{}", player[0].val()),
+                       format!("{:?}", dealer[0]),
+                       format!("{}", dealer[0].val()),
+                       format!("{:?}", player[1]),
+                       format!("{}", player[1].val()), 
+                       format!("{:?}", dealer[1]),
+                       format!("{}", dealer[1].val()),
+                       format!("{}", player_sum),
+                       format!("{}", dealer_sum)];
+        wtr.write_record(&rec)?;
+        //println!("{:?}\n{:?}\n", player, dealer);
+    }
+    Ok(())
 }
